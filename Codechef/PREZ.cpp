0a87b1e86195d38c419d6112803d93096326abe3
Ishan Sharma <1905614@kiit.ac.in>
2022-01-29 21:58:03 +0530

    2nd q comitted

--- Codechef/PREZ.cpp
+++ Codechef/PREZ.cpp
@@ -1,30 +1,51 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define pb push_back
-#define ff first
-#define ss second
-#define f(n) for (int i = 0; i < n; i++)
-#define inf 1e18
-#define mid(l, r) (l + (r - l) / 2)
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef map<int, int> mii;
-typedef unordered_map<int, int> ump;
-#define w(x)  \
-    int x;    \
-    cin >> x; \
-    while (x--)
+#define int long long int
+#define ld long double
+#define w(x)  int x; cin>>x; while(x--)
+ int n,k;
+ string s;
 
+int checker(int mid)
+{
+    int sum=0;
+    for (int i = mid-1; i >=0; i--)
+    {
+        int z=(sum+long(s[i])-48)%10;
+        if(z==0) continue;
+        else{
+            sum+=(10-z);
+        }
+    }
+    if(sum<=k)
+        return 1;
+    return 0;
+    
+}
 void solve()
 {
-
+    cin>>n>>k;
+    cin>>s;
+    int lo=0,hi=n,mid,ans=0;
+    while(lo<=hi){
+        mid=(lo+hi)/2;
+        if(checker(mid))
+        {
+            lo=mid+1;
+            ans=mid;
+        }else{
+            hi=mid-1;
+        }
+    }
+    cout<<ans<<endl;
 }
+
 int32_t main()
 {
-    w(t)
-    {
-        solve();
-    }
-
+   w(t)
+   {
+       solve();
+   }
+ 
     return 0;
 }
