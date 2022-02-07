e9a6bab3cf6cdb88fe21db5a6603696f2a0573f6
Ishan Sharma <1905614@kiit.ac.in>
2022-02-07 23:47:10 +0530

    tree rev

--- Binary Tree/BT.cpp
+++ Binary Tree/BT.cpp
@@ -1,22 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
-    Node(int val){
-        data=val;
-        left=NULL;
-        right=NULL;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
     }
 };
+void pre(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    pre(root->left);
+    pre(root->right);
+}
+void post(struct Node *r)
+{
+    if (r == NULL)
+        return;
+    post(r->left);
+    post(r->right);
+    cout << r->data << " ";
+}
+void in(struct Node *r)
+{
+    if (r == NULL)
+        return;
+    in(r->left);
+    cout << r->data << " ";
+    in(r->right);
+}
 int main()
 {
-    struct Node* root=new Node(1);
-    root->left=new Node(2);
-    root->right=new Node(3);
-    root->left->left=new Node(4);
-    root->left->right=new Node(5);
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    cout << "Pre order : ";
+    pre(root);
+    cout << "\nIn order : ";
+    in(root);
+    cout << "\nPost order : ";
+    post(root);
     return 0;
 }
