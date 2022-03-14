a8be997ed99d66b8e1ea10dbe7b54aa7c9f1984f
Ishan Sharma <1905614@kiit.ac.in>
2022-03-14 23:49:53 +0530

    leetcode pbds problem

---
+++ PBDS/leetcode.cpp
@@ -0,0 +1,6 @@
+//https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less_equal<>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
