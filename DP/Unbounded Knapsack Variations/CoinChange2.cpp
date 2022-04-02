4f2c668cb53ed93e5e8f59ab335b03de131bfa91
Ishan Sharma <1905614@kiit.ac.in>
2022-04-02 23:31:54 +0530

    dp update

---
+++ DP/Unbounded Knapsack Variations/CoinChange2.cpp
@@ -0,0 +1,12 @@
+/*
+IMPORTANT / UNIQUE PROBLEM
+MIN NO. OF COINS
+I/P= coin[]={1,2,3} and sum=5
+O/P= 2
+2+3-->2 (min number of coins)
+1+1+3->3
+1+2+2->3
+1+1+1+1+1->5
+1+1+1+2->4
+since each coin can be chosen multiple times(infinite supply of coins) ----> unbounded (wt[]->coin[] and W->sum)
+*/
