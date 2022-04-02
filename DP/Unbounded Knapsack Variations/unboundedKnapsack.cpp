0e20fe0e498e9c1d4fa320b5326a75b363ffc0e5
Ishan Sharma <1905614@kiit.ac.in>
2022-04-02 19:39:54 +0530

    unbounded topdown

--- DP/Unbounded Knapsack Variations/unboundedKnapsack.cpp
+++ DP/Unbounded Knapsack Variations/unboundedKnapsack.cpp
@@ -2,13 +2,13 @@
 Multiple occurances of the same item:
 given W, n, value[] and wt[]
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int N=1e5+10;
+const int N = 1e5 + 10;
 static int dp[101][1001];
 /*
 int knapsackRecursion(int n,int W,vector<int>val,vector<int>wt){
-    //base condition 
+    //base condition
     if(n==0 || W==0) return 0;
     //choice diagram
     if(wt[n-1]<=W)
@@ -17,8 +17,9 @@ int knapsackRecursion(int n,int W,vector<int>val,vector<int>wt){
     return knapsackRecursion(n-1,W,val,wt);
 }
 */
+/*
 int knapsackMemoize(int n,int W,vector<int>val,vector<int>wt){
-    //base condition 
+    //base condition
     if(n==0 || W==0) return 0;
     //memoize
     if(dp[n][W]!=-1) return dp[n][W];
@@ -28,18 +29,44 @@ int knapsackMemoize(int n,int W,vector<int>val,vector<int>wt){
     else if(wt[n-1]>W)
     return dp[n][W]=knapsackMemoize(n-1,W,val,wt);
 }
+*/
+int knapsackTopDown(int n, int W, vector<int> val, vector<int> wt)
+{
+    // base condition
+    if (n == 0 || W == 0)
+        return 0;
+    // choice diagram
+    for (int i = 1; i < n + 1; i++)
+        for (int j = 1; j < W + 1; j++)
+        {
+            if (wt[n - 1] <= W)
+                dp[i][j] = max(val[i] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    return dp[n][W];
+}
 int main()
 {
-    int W,n;printf("Enter the number of items and the capacity of the knapsack:\n");
-    scanf("%d%d",&n,&W);
-    vector<int>wt(n);
-    vector<int>val(n);
-    for(int i=0; i < n; i++){
-        cout<<"Enter weight and value of item #"<<i+1<<endl;
-        cin>>wt[i]>>val[i];
+    int W, n;
+    printf("Enter the number of items and the capacity of the knapsack:\n");
+    scanf("%d%d", &n, &W);
+    vector<int> wt(n);
+    vector<int> val(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter weight and value of item #" << i + 1 << endl;
+        cin >> wt[i] >> val[i];
     }
-    dp[n+1][W+1];
-    memset(dp, -1, sizeof(dp));
-    cout<<"Max profit : "<<knapsackMemoize(n,W,val,wt)<<endl;
+    dp[n + 1][W + 1];
+    for (int i = 0; i < n + 1; i++)
+        for (int j = 0; j < W + 1; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    cout << "Max profit : " << knapsackTopDown(n, W, val, wt) << endl;
     return 0;
 }
